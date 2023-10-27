98d7108908677ba12f16a425186d1b2acd214064
legerakun <pavel457pro@mail.ru>
2023-10-27 14:46:27 +0800

    Changed source code link

--- src/home.js
+++ src/home.js
@@ -24,22 +24,16 @@ export function Home() {
 	addText(home, 
 		`This is a single-page website created from scratch. 
 		Here I'm testing some opportunities in HTML, CSS, and JavaScript.
-		Feel free to use the <a href="index.html" class="font-link"> source code </a>.`,
+		Feel free to use the <a href="https://github.com/legerakun/portfolio-js" target="_blank" class="font-link">source code</a>.`,
 		`Это одностраничный сайт созданный с нуля.
 		Здесь я тестирую различные возможности HTML, CSS и JavaScript.
-		Здесь вы можете найти <a href="index.html" class="font-link"> исходный код </a>.`
+		Здесь вы можете найти <a href="https://github.com/legerakun/portfolio-js" target="_blank" class="font-link">исходный код</a>.`
 	);
 
 	const landing = addElement("img", containerRight);
 	landing.className = "landing";
 
 	applyProperties();
-
-	const fontLink = document.querySelector(".font-link");
-	fontLink.addEventListener("click", function() {
-		history.pushState({page: 2}, "");
-		About();
-	}, false);
 
 	const containerMarginBottom = addElement("container-margin", container);
 	containerMarginBottom.style.minHeight = "140px";
