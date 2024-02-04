cba4f950228637dba1d3e1756323b7360dde111a
legerakun <pavel457pro@mail.ru>
2024-02-04 20:23:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
   build: {
     target: "esnext"
-  }
-}
+  },
+  base: "https://legerakun.github.io/portfolio/",
+});
